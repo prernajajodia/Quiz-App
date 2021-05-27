5f422944efb6b6b4da20d1138fb8aef62829d2a8
Prerna Jajodia <jajodiapj@gmail.com>
2021-05-27 20:07:53 +0530

    Login intro layout created

---
+++ app/src/main/java/com/prernajajodia/quizapp/LoginIntroActivity.kt
@@ -0,0 +1,13 @@
+package com.prernajajodia.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginIntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_intro)
+
+
+    }
+}
