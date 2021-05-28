7a18c975276e6cdf3cd145b5addb405fae325531
Prerna Jajodia <jajodiapj@gmail.com>
2021-05-28 16:29:30 +0530

    Refactored and login and sign up layout created

--- app/src/main/java/com/prernajajodia/quizapp/LoginIntroActivity.kt
+++ app/src/main/java/com/prernajajodia/quizapp/LoginIntroActivity.kt
@@ -1,12 +1,24 @@
 package com.prernajajodia.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.prernajajodia.quizapp.databinding.ActivityLoginIntroBinding
 
 class LoginIntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_intro)
+
+        binding = ActivityLoginIntroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.getStartedBtn.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
 
 
     }
