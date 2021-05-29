00dc6d0d76475ed77b283727190113fbada069a4
Prerna Jajodia <jajodiapj@gmail.com>
2021-05-29 15:11:17 +0530

    Firebase authentication sign up done

--- app/src/main/java/com/prernajajodia/quizapp/LoginActivity.kt
+++ app/src/main/java/com/prernajajodia/quizapp/LoginActivity.kt
@@ -2,10 +2,18 @@ package com.prernajajodia.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.prernajajodia.quizapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
     }
 }
--- app/src/main/java/com/prernajajodia/quizapp/SignUpActivity.kt
+++ app/src/main/java/com/prernajajodia/quizapp/SignUpActivity.kt
@@ -1,11 +1,57 @@
 package com.prernajajodia.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.prernajajodia.quizapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.signupBtn.setOnClickListener {
+            signUp()
+        }
     }
+
+    private fun signUp() {
+        val email = binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        val confirmPass = binding.etConfirmPassword.text.toString()
+
+        if (email.isBlank() || password.isBlank()) {
+            Toast.makeText(this, "Email or password is blank!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password != confirmPass) {
+            Toast.makeText(this, "Passwords do not match! Check again", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, getString(R.string.sign_up_error_toast), Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+    }
+
 }
