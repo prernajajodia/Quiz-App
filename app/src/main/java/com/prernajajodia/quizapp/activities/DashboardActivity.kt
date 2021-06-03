cd6349809a31ce4ca61530d0e4666ff1176cf014
Prerna Jajodia <jajodiapj@gmail.com>
2021-06-04 04:17:20 +0530

    Firestore integration done using dummy data

--- app/src/main/java/com/prernajajodia/quizapp/activities/DashboardActivity.kt
+++ app/src/main/java/com/prernajajodia/quizapp/activities/DashboardActivity.kt
@@ -1,11 +1,14 @@
 package com.prernajajodia.quizapp.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.prernajajodia.quizapp.R
 import com.prernajajodia.quizapp.activities.adapters.QuizAdapter
 import com.prernajajodia.quizapp.activities.models.Quiz
@@ -16,6 +19,8 @@ class DashboardActivity : AppCompatActivity() {
     lateinit var adapter: QuizAdapter
     lateinit var binding: ActivityDashboardBinding
     lateinit var firebaseAuth: FirebaseAuth
+
+    lateinit var firebaseFirestore: FirebaseFirestore
 
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
@@ -30,10 +35,24 @@ class DashboardActivity : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
 
+        setUpFireStore()
         setUpDrawerLayout()
         populateDummyData()
         setUpRecyclerView()
 
+    }
+
+    private fun setUpFireStore() {
+
+        firebaseFirestore = FirebaseFirestore.getInstance()
+        val collectionReference = firebaseFirestore.collection("quizzes")
+        collectionReference.addSnapshotListener { value, error ->
+            if (value == null || error!= null){
+                Toast.makeText(this, "Data fetching error", Toast.LENGTH_LONG).show()
+                return@addSnapshotListener
+            }
+            Log.d("DATA", value.toObjects(Quiz::class.java).toString())
+        }
     }
 
     private fun populateDummyData() {
